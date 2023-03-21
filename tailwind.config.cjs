3a8569111241c294c1bdf38c54a40fb82bea73f1
Kai Takami <kaitakami@protonmail.com>
2023-03-21 20:24:06 +0900

    Install tremor beta

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
+    // Path to the tremor module
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
