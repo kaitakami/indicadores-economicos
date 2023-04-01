02017a1aad31679824b8aa8bba81b8506a167c1b
Kai Takami <kaitakami@protonmail.com>
2023-04-01 10:49:30 +0900

    Install eslint standard config and run eslint --fix

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import Layout from "./components/Layout";
+import Layout from './components/Layout'
 
 const App = () => {
-  return (
-    <Layout>
-      { /* Aquí va el contenido de la applicación */ }
-    </Layout>
-  );
+  return <Layout>{/* Aquí va el contenido de la applicación */}</Layout>
 }
 
 export default App
