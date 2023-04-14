8e1372712761e9a29bfb7c4cf1cef361f1bd0cf1
Kai Takami <kaitakami@protonmail.com>
2023-04-15 08:50:45 +0900

    Change font weight in title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 function Header() {
 	return (
 		<header className="p-2 text-lg text-center md:p-4">
-			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl pt-5">
+			<h1 className="scroll-m-20 text-4xl font-medium sm:font-semibold tracking-tight lg:text-5xl text-left sm:text-center py-5">
 				Indicadores Económicos
 			</h1>
+			<hr className="border-stone-600" />
 		</header>
 	)
 }
